6f6297c4e11abba4ef082fece9e9cf2d5a16a2a5
효정이 <w2029@e-mirim.hs.kr>
2021-05-25 19:28:51 +0900

    Completion Project5_2 commit

--- app/src/main/java/kr/hs/emirim/w2029/project5_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/project5_2/MainActivity.java
@@ -11,14 +11,16 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
     TextView textResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int[] btnIds = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9};
         Button[] btnNums = new Button[btnIds.length];
-        for(int i = 0; i<btnNums.length; i++) {
+        for (int i = 0; i < btnNums.length; i++) {
             btnNums[i] = findViewById(btnIds[i]);
+            btnNums[i].setOnClickListener(btnNumListener);
         }
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             int num1 = Integer.parseInt(edit1.getText().toString());
             int num2 = Integer.parseInt(edit2.getText().toString());
             int result = 0;
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.btn_plus:
                     result = num1 + num2;
                     break;
@@ -56,6 +58,17 @@ public class MainActivity extends AppCompatActivity {
             }
             textResult.setText(R.string.text_result);
             textResult.append(" " + result);
+        }
+    };
+    View.OnClickListener btnNumListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button btn = (Button) v;
+            if (edit1.isFocused()) {
+                edit1.setText(edit1.getText().toString() + btn.getText());
+            } else {
+                edit2.setText(edit2.getText().toString() + btn.getText());
+            }
         }
     };
 }
